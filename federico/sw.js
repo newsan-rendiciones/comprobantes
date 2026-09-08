da7c2a3c0d9fe7ecfdf67c48714a60c3c001efb3
newsan-rendiciones <agustina.jorba@newpga.com.ar>
2026-09-08 11:22:58 -0300

    Add files via upload

---
+++ federico/sw.js
@@ -0,0 +1,19 @@
+/* Service worker de la app de Fede (scope: /fede/) */
+const CACHE = 'comprobantes-fede-v1';
+const SHELL = ['./', './index.html', './manifest.json', '../icon-192.png', '../icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(SHELL).catch(function () {}); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) { if (k !== CACHE) return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function (e) {
+  var url = e.request.url;
+  if (url.indexOf('script.google.com') !== -1 || url.indexOf('googleusercontent.com') !== -1) return;
+  e.respondWith(caches.match(e.request).then(function (hit) { return hit || fetch(e.request); }));
+});
